76da8ce5139af054dfe0b31f292b85bd3b242c1c
joelsonlins <joelsonlins@hotmail.com>
2025-11-23 20:20:32 -0300

    Finalizando fundamentos

---
+++ 00 - Fundamentos/string_1.py
@@ -0,0 +1,21 @@
+nome = "carLOS"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "   Olá Mundo!   "
+
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
+
+menu = "Python"
+
+print("####" + menu + "####")
+print("center14")
+print(menu.center(14))
+print("center14+#")
+print(menu.center(14, "#"))
+print("-".join(menu))
