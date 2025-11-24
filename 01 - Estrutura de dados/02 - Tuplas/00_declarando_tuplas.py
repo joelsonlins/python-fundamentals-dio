f7f073f9c347c93b9ce228c666d587a178dcac6d
joelsonlins <joelsonlins@hotmail.com>
2025-11-24 09:54:12 -0300

    introducao a tuplas

---
+++ 01 - Estrutura de dados/02 - Tuplas/00_declarando_tuplas.py
@@ -0,0 +1,11 @@
+frutas = ("banana", "manga", "melancia",)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
